b4215220039054b6bcfacf823bdb534f0a1e3a60
Durvesh Vedak <durveshvedak@Durveshs-MBP.fios-router.home>
2019-04-28 10:15:04 -0400

    * nyc dataframe

--- src/scraper.py
+++ src/scraper.py
@@ -1,14 +1,50 @@
-import bs4 as bs
 import urllib.request
+from bs4 import BeautifulSoup
+import pandas as pd
 
-source = urllib.request.urlopen('http://www.city-data.com/accidents/acc-Cincinnati-Ohio.html').read()
-soup = bs.BeautifulSoup(source,'lxml')
-table = soup.find('table')
-table_rows = table.find_all('tr')
-print(table_rows)
-for tr in table_rows:
-    td = tr.find_all('td')
-    row = [i.text for i in td]
-    print(row)
+
+webpage="http://www.city-data.com/accidents/acc-Cincinnati-Ohio.html"
+page = urllib.request.urlopen(webpage)
+soup = BeautifulSoup(page)
+fatal_car_crashes = pd.DataFrame(columns = ['YEAR', 'TYPE','COUNT'])
+
+for li in soup.find_all("li", class_="list-group-item active"):
+    print(li.contents)
+
+for b in soup.find_all("b"):
+    print(b.contents)
+
+for s in soup.find_all("span", class_="badge"):
+    print(s.contents)
+
+fatal_car_crashes.YEAR = [2016,2016,2016,2016,2016,2016,2017,2017,2017,2017,2017,2017]
+fatal_car_crashes.TYPE = ['Fatal accident count',
+'Vehicles involved in fatal accidents',
+'Fatal accidents involving drunken persons',
+'Fatalities',
+'Persons involved in fatal accidents',
+'Pedestrians involved in fatal accidents',
+'Fatal accident count',
+'Vehicles involved in fatal accidents',
+'Fatal accidents involving drunken persons',
+'Fatalities',
+'Persons involved in fatal accidents',
+'Pedestrians involved in fatal accidents']
+fatal_car_crashes.COUNT=['22',
+'38',
+'7',
+'26',
+'47',
+'7',
+'31',
+'50',
+'11',
+'31',
+'56',
+'8',
+]
+
+print(fatal_car_crashes)
+
 
 
